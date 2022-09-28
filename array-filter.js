962770e55baa1f7c94388aa460a4b9aae60e2993
Luis <gutierrezrivera@gmail.com>
2022-09-28 18:54:54 -0400

    initial commit

---
+++ array-filter.js
@@ -0,0 +1,9 @@
+const arr = [1,2,3,4,5,6];
+var length = arr.length;
+var i = 0;
+
+do {
+    console.log(arr[i]);
+    i++;
+}
+while (i < 5);
---
+++ array-map.js
@@ -0,0 +1,21 @@
+ function myFunction() {
+    
+
+}
+
+function funtionDeux(){
+
+    
+
+}
+
+
+let myArray = [1, 2, 3, 4];
+
+for(let i = 0; i < myArray.length; i++){ 
+
+console.log(myArray[i]);
+
+}
+
+
